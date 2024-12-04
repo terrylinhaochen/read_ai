1aba2a63ebd1f8bc8ca61eabe99084dc39a6aa21
TerryChenRDFZ <terrylinhaochen@163.com>
2024-12-03 23:57:33 -0600

    cleaned up files

--- book-learning-app/tailwind.config.js
+++ book-learning-app/tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
-
